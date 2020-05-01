45a65f2fb221471ab03f10ebec9fcec55297d332
jock.lawrie <jock.lawrie@gmail.com>
2020-05-01 13:09:39 +1000

    Progress with school contacts.

--- src/abm/contacts.jl
+++ src/abm/contacts.jl
@@ -13,28 +13,72 @@ function populate_contacts!(agents, cfg, indata)
     d_nadults_without_children = Categorical([0.42, 0.47, 0.04, 0.04, 0.02, 0.01])
     workplace_sizes  = Dict(1 => 2, 2 => 10, 3 => 50, 4 => 200, 5 => 500, 6 => 100, 7 => 2000)
     d_workplace_size = Categorical([0.25, 0.25, 0.25, 0.15, 0.05, 0.03, 0.02])  # Categories are: 2, 10, 50, 200, 500, 1000, 2000
-    class_size = 20
+    d_max_nstudents_per_level = Categorical([0.1, 0.4, 0.4, 0.1])  # Categories are: 15, 20, 25, 30
 
     populate_households!(agents, age2first, d_nparents, d_nchildren, d_nadults_without_children)
-    populate_school_contacts!()
+    populate_school_contacts!(agents, age2first, d_max_nstudents_per_level)
     populate_workplace_contacts!(agents, cfg.n_workplace_contacts, d_workplace_size, workplace_sizes)
     populate_community_contacts!(agents, cfg.n_community_contacts)
     populate_social_contacts!(agents, cfg.n_social_contacts)
 end
 
-function append_contact!(agent, category::Symbol, id::Int)
-    id == agent.id && return  # A person is not a contact of him/herself
-    v = getproperty(agent, category)  # Vector{Int}
-    push!(v, id)
+################################################################################
+# Utils
+
+function append_contact!(agentid, contactid::Int, contactlist::Vector{Int})
+    agentid == contactid && return
+    push!(contactlist, contactid)
 end
 
-function append_contacts!(agent, category::Symbol, ids::Vector{Int})
-    v = getproperty(agent, category)  # Vector{Int}
-    ref_id = agent.id
-    for id in ids
-        id == ref_id && continue  # A person is not a contact of him/herself
-        push!(v, id)
+"Append contactids to agent.category"
+function append_contacts!(agent, category::Symbol, contactids::Vector{Int})
+    agentid = agent.id
+    contactlist = getproperty(agent, category)  # Vector{Int}
+    for contactid in contactids
+        append_contact!(agentid, contactid, contactlist)
     end
+end
+
+"Randomly assign ncontacts to each agent in agents."
+function assign_contacts_regulargraph!(agents, contactcategory::Symbol, ncontacts::Int)
+    npeople   = length(agents)
+    ncontacts = ncontacts > npeople - 1 ? npeople - 1 : ncontacts
+    g         = random_regular_graph(npeople, ncontacts)  # npeople (vertices) each with ncontacts (edges to ncontacts other vertices)
+    adjlist   = g.fadjlist
+    for agent in agents
+        setproperty!(agent, contactcategory, adjlist[agent.id])
+    end
+end
+
+"Randomly assign ncontacts to each agent whose id is a value of vertexid2agentid."
+function assign_contacts_regulargraph!(agents, contactcategory::Symbol, ncontacts::Int, vertexid2agentid)
+    nvertices = length(vertexid2agentid)
+    ncontacts = ncontacts > nvertices - 1 ? nvertices - 1 : ncontacts
+    g = random_regular_graph(nvertices, ncontacts)  # nvertices each with ncontacts (edges to ncontacts other vertices)
+    adjlist = g.fadjlist
+    for (vertexid, agentid) in vertexid2agentid
+        contactlist_vertex = adjlist[vertexid]  # Contact list as vertexid domain...convert to agentid domain
+        contactlist_agent  = getproperty(agents[agentid], contactcategory)
+        for vertexid in contactlist_vertex
+            append_contact!(agentid, vertexid2agentid[vertexid], contactlist_agent)
+        end
+    end
+end
+
+"""
+Return the id of a random agent whose is unplaced and in the specified age range.
+If one doesn't exist return a random id from unplaced_people.
+"""
+function sample_person(unplaced_people::Set{Int}, min_age, max_age, agents, age2first)
+    i1 = age2first[min_age]
+    i2 = age2first[max_age + 1] - 1
+    s  = i1:i2      # Indices of people in the age range
+    n  = length(s)  # Maximum number of random draws
+    for i = 1:n
+        id = rand(s)  # id is a random person in the age range
+        id in unplaced_people && return id  # id is also an unplaced person
+    end
+    rand(unplaced_people)
 end
 
 """
@@ -144,7 +188,7 @@ function populate_households_with_children!(agents, age2first, hh2count, d_npare
         max_age = 17  # Maximum age of the next selected child
         age_youngest_child = 1000
         age_oldest_child   = -1
-        for i = 1:nc
+        for j = 1:nc
             child_id = sample_person(unplaced_children, min_age, max_age, agents, age2first)
             pop!(unplaced_children, child_id)
             push_child!(hh, child_id)
@@ -158,7 +202,7 @@ function populate_households_with_children!(agents, age2first, hh2count, d_npare
         # Select parent/s
         min_parent_age = age_oldest_child + 20
         max_parent_age = age_oldest_child + 45
-        for i = 1:np
+        for j = 1:np
             parent_id = sample_person(unplaced_parents, min_parent_age, max_parent_age, agents, age2first)
             pop!(unplaced_parents, parent_id)
             push_adult!(hh, parent_id)
@@ -195,7 +239,7 @@ function populate_households_without_children!(agents, age2first, hh2count, d_na
         hh = Household(na, 0)
 
         # Select adult/s
-        for i = 1:na
+        for j = 1:na
             adult_id = rand(unplaced_adults)
             pop!(unplaced_adults, adult_id)
             push_adult!(hh, adult_id)
@@ -208,19 +252,6 @@ function populate_households_without_children!(agents, age2first, hh2count, d_na
         # Stopping criteria
         isempty(unplaced_adults) && break
     end
-end
-
-"Returns the id of an agent in the age range if one exists, else returns a random id from ids."
-function sample_person(unplaced_people::Set{Int}, min_age, max_age, agents, age2first)
-    i1 = age2first[min_age]
-    i2 = age2first[max_age + 1] - 1
-    s  = i1:i2      # Indices of people in the age range
-    n  = length(s)  # Maximum number of random draws
-    for i = 1:n
-        id = rand(s)  # id is a random person in the age range
-        id in unplaced_people && return id  # id is also an unplaced person
-    end
-    rand(unplaced_people)
 end
 
 function set_household_contacts!(agents, household)
@@ -239,19 +270,137 @@ end
 ################################################################################
 # School contacts for teachers and people under 23
 
-mutable struct ChildCareCentre
+"""
+Each teacher can contact N1 other teachers and N2 students from any age group.
+Each student can contact N3 students in their age group.
+"""
+mutable struct School
     max_nteachers::Int
-    max_children_per_level::Int
+    max_nstudents_per_level::Int
     teachers::Vector{Int}
-    level2children::Dict{Int, Vector{Int}}  # age => [childid1, ...]
+    age2students::Dict{Int, Vector{Int}}  # Classes: age => [childid1, ...]
+
+    function School(max_nteachers, max_nstudents_per_level, teachers, age2students)
+        teacher2student_ratio = 1 / 15  # Need at least 1 teacher to 15 students
+        nlevels       = length(age2students)
+        max_nstudents = nlevels * max_nstudents_per_level
+        min_nteachers = round(Int, teacher2student_ratio * max_nstudents)
+        max_nteachers < min_nteachers && error("Not enough teachers")  # Need 1.5 per level, 6 levels
+        max_nstudents_per_level < 5    && error("max_nstudents_per_level must be at least 5")
+        new(max_nteachers, max_nstudents_per_level, teachers, age2students)
+    end
 end
 
-function isfull(cc::ChildCareCentre)
-    
+function School(schooltype::Symbol, max_nstudents_per_level)
+    # Construct age2students
+    if schooltype == :childcare
+        age2students = Dict(age => Int[] for age = 0:4)
+    elseif schooltype == :primary
+        age2students = Dict(age => Int[] for age = 5:11)
+    elseif schooltype == :secondary
+        age2students = Dict(age => Int[] for age = 12:17)
+    elseif schooltype == :tertiary
+        age2students = Dict(age => Int[] for age = 18:23)
+    else
+        error("Unknown school type")
+    end
+
+    # Calculate the number of required teachers
+    teacher2student_ratio = 1 / 15  # Need at least 1 teacher to 15 students
+    nlevels       = length(age2students)
+    max_nstudents = nlevels * max_nstudents_per_level
+    max_nteachers = round(Int, teacher2student_ratio * max_nstudents)
+    School(max_nteachers, max_nstudents_per_level, Int[], age2students)
+end
+
+function isfull(school::School)
+    length(school.teachers) >= school.max_nteachers && return true
+    for (age, students) in school.age2students
+        length(students) < school.max_nstudents_per_level && return false  # Room in this age group
+    end
+    true
+end
+
+function push_teacher!(school::School, id::Int)
+    length(school.teachers) >= school.max_nteachers && return false  # Teacher positions are full. No success.
+    push!(school.teachers, id)  # Add teacher to school
+    true  # Success
+end
+
+function push_student!(school::School, id::Int, age::Int)
+    v = school.age2students[age]
+    length(v) >= school.max_nstudents_per_level && return false  # Child positions are full. No success.
+    push!(v, id)  # Add teacher to school
+    true  # Success
+end
+
+function populate_school_contacts!(agents, age2first, d_max_nstudents_per_level)
+    min_teacher_age   = 24
+    max_teacher_age   = 65
+    unplaced_students = Dict(age => Set(age2first[age]:(age2first[age+1] - 1)) for age = 0:23)
+    unplaced_teachers = Set((age2first[min_teacher_age]):(age2first[max_teacher_age] - 1))
+    imax              = length(unplaced_students)
+    for i = 1:imax  # Cap the number of iterations by placing at least 1 child per iteration
+        # Init school
+        agentid = nothing
+        for age = 0:23
+            isempty(unplaced_students[age]) && continue
+            agentid = rand(unplaced_students[age])
+            break
+        end
+        isnothing(agentid) && break  # STOPPING CRITERION: There are no unplaced students remaining
+        student    = agents[agentid]
+        schooltype = determine_schooltype(student.age)
+        school     = School(schooltype, rand(d_max_nstudents_per_level))
+
+        # Fill student positions
+        age2students            = school.age2students
+        max_nstudents_per_level = school.max_nstudents_per_level
+        for (age, students) in age2students
+            n_available = max_nstudents_per_level - length(students)  # Number of available positions
+            for j = 1:n_available
+                isempty(unplaced_students[age]) && break
+                studentid = sample_person(unplaced_students[age], age, age, agents, age2first)
+                pop!(unplaced_students[age], studentid)
+                push_student!(school, studentid)
+            end
+        end
+
+        # Fill teacher positions
+        n_available = school.max_nteachers - length(school.teachers)  # Number of available positions
+        for j = 1:n_available
+            isempty(unplaced_teachers) && break
+            teacherid = sample_person(unplaced_teachers, min_teacher_age, max_teacher_age, agents, age2first)
+            pop!(unplaced_teachers, teacherid)
+            push_teacher!(school, teacherid)
+        end
+
+        # Set contact lists
+        set_student_to_student_contacts!()
+        set_teacher_to_student_contacts!()
+        set_teacher_to_teacher_contacts!()
+    end
+end
+
+function determine_schooltype(age::Int)
+    age <= 4  && return :childcare
+    age <= 11 && return :primary
+    age <= 17 && return :secondary
+    age <= 23 && return :tertiary
+    error("Person with age $(age) cannot be assigned as a student to a school")
+end
+
+function set_student_to_student_contacts!(agents, school::School, ncontacts_s2s::Int)
+    age2students = school.age2students
+    for (age, students) in age2students
+        append_contacts!(agents[studentid], :workplace, adults)
+
+        assign_contacts_regulargraph!(agents, :workplace, min(ncontacts_s2s, workplace_size), adultid2agentid)
+    end
 end
 
 ################################################################################
-# Workplace contacts
+# Workplace, community and social contacts
 
 function populate_workplace_contacts!(agents, ncontacts::Int, d_workplace_size, workplace_sizes::Dict{Int, Int})
     nagents = size(agents, 1)
@@ -264,7 +413,7 @@ function populate_workplace_contacts!(agents, ncontacts::Int, d_workplace_size, 
         adultid += 1
         adultid2agentid[adultid] = agent.id
         if adultid == workplace_size
-            assign_workplace_contacts!(agents, min(ncontacts, workplace_size), adultid2agentid)
+            assign_contacts_regulargraph!(agents, :workplace, min(ncontacts, workplace_size), adultid2agentid)
             adultid = 0
             adultid2agentid = Dict{Int, Int}()
             workplace_size  = workplace_sizes[rand(d_workplace_size)]
@@ -272,37 +421,7 @@ function populate_workplace_contacts!(agents, ncontacts::Int, d_workplace_size, 
     end
 end
 
-function assign_workplace_contacts!(agents, ncontacts::Int, adultid2agentid)
-    g = random_regular_graph(length(adultid2agentid), ncontacts)  # nadults (vertices) each with ncontacts (edges to ncontacts other vertices)
-    adjlist = g.fadjlist
-    for (adultid, agentid) in adultid2agentid
-        contactlist = adjlist[adultid]  # Contact list as adultid domain...convert to agentid domain
-        agent = agents[agentid]
-        for id in contactlist
-            push!(agent.workplace, adultid2agentid[id])
-        end
-    end
-end
-
-################################################################################
-# Social contacts
-
-function populate_community_contacts!(agents, ncontacts::Int)
-    npeople = length(agents)
-    g = random_regular_graph(npeople, ncontacts)  # npeople (vertices) each with ncontacts (edges to ncontacts other vertices)
-    adjlist = g.fadjlist
-    for id = 1:npeople
-        agents[id].community = adjlist[id]
-    end
-end
-
-function populate_social_contacts!(agents, ncontacts::Int)
-    npeople = length(agents)
-    g = random_regular_graph(npeople, ncontacts)  # npeople (vertices) each with ncontacts (edges to ncontacts other vertices)
-    adjlist = g.fadjlist
-    for id = 1:npeople
-        agents[id].social = adjlist[id]
-    end
-end
+populate_community_contacts!(agents, ncontacts) = assign_contacts_regulargraph!(agents, :community, ncontacts)
+populate_social_contacts!(agents, ncontacts)    = assign_contacts_regulargraph!(agents, :social,    ncontacts)
 
 end
