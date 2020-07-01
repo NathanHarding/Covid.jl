99c389a2fff1e9e6e334293eae6e052d313a90a4
jock.lawrie <jock.lawrie@gmail.com>
2020-07-01 18:24:18 +1000

    Implemented a version of Beaumont (2009) for trainmodel.

--- src/Covid.jl
+++ src/Covid.jl
@@ -4,7 +4,8 @@ include("run_model.jl")  # Exports: runmodel
 using .run_model
 
 #include("train_model.jl")  # Exports: trainmodel
-include("train_model_abc.jl")  # Exports: trainmodel
+#include("train_model_abc.jl")  # Exports: trainmodel
+include("train_model_abc_beaumont.jl")  # Exports: trainmodel
 using .train_model
 
 end
