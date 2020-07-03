b48a277c4669b8e3fa4a189ee6003a3ae4f1f294
jock.lawrie <jock.lawrie@gmail.com>
2020-07-03 17:20:25 +1000

    Rewrote the mapping between parameters and particles. Enabled priors to be specified in YAML.

--- src/train_model_abc1p4.jl
+++ src/train_model_abc1p4.jl
@@ -15,14 +15,13 @@ using YAML
 include("abm.jl")  # Depends on: core, config, contacts
 using .abm
 
-# Convenience store
-const store = Dict{Symbol, Any}(:y => Int[], :model => nothing, :config => nothing, :metrics => nothing, :unknowns => nothing, :ymax => 0)
+const store = Dict{Symbol, Any}()  # Used in the loss function
 
 function trainmodel(configfile::String)
     @info "$(now()) Configuring model"
     d   = YAML.load_file(configfile)
     cfg = Config(d)
-    unknowns, n_unknowns = construct_unknowns(d["unknowns"])
+    name2prior = construct_name2prior(d["unknowns"])
 
     @info "$(now()) Preparing training data"
     y = prepare_training_data(d)
@@ -32,141 +31,70 @@ function trainmodel(configfile::String)
     model  = init_model(params, cfg)
 
     @info "$(now()) Populating convenience store"
-    store[:y]        = y
-    store[:ymax]     = maximum(y)
-    store[:model]    = model
-    store[:config]   = cfg
-    store[:metrics]  = metrics
-    store[:unknowns] = unknowns
+    store[:y]          = y
+    store[:ymax]       = maximum(y)
+    store[:model]      = model
+    store[:config]     = cfg
+    store[:metrics]    = metrics
+    store[:name2prior] = name2prior
 
     @info "$(now()) Training model"
     opts  = Dict{Symbol, Any}(Symbol(k) => v for (k, v) in d["options"])
     sd    = pop!(opts, :std)  # Standard deviation of the loss
-    prior = Factored([Uniform(0.0,0.05), Uniform(0.0,0.5), Uniform(0.0,0.5), Uniform(0.0,0.5), Uniform(0.0,0.5), Uniform(0.0,0.5), Uniform(0.0,0.5)]...)
+    prior = Factored([dist for (name, dist) in name2prior]...)
     approxdensity = ApproxKernelizedPosterior(prior, loss, sd)
     particles, loglik = mcmc(approxdensity; opts...)
 
     @info "$(now()) Extracting result"
-    result  = construct_result(particles, loglik, sd)
-    outfile = joinpath(cfg.output_directory, "trained_params.tsv")
+    colnames = [Symbol(colname) for (colname, prior) in name2prior]
+    result   = construct_result(particles, loglik, sd, colnames)
+    outfile  = joinpath(cfg.output_directory, "trained_params.tsv")
     CSV.write(outfile, result; delim='\t')
     @info "$(now()) Finished"
 end
 
 ################################################################################
-# Construct unknowns
+# Construct paramname => prior (orderd); Map a particle to params and policies
 
-function construct_unknowns(d)
-    result     = Dict{Symbol, Any}()
-    n_unknowns = format_param_unknowns!(result, 0, d)
-    n_unknowns = format_policy_unknowns!(result, n_unknowns, d, "distancing_policy")
-    n_unknowns = format_policy_unknowns!(result, n_unknowns, d, "testing_policy")
-    n_unknowns = format_policy_unknowns!(result, n_unknowns, d, "tracing_policy")
-    n_unknowns = format_policy_unknowns!(result, n_unknowns, d, "quarantine_policy")
-    result, n_unknowns
+const registered_priors = Dict("uniform" => Uniform, "Normal" => Normal)
+
+function construct_name2prior(arr)
+    result = Pair{String, Distribution}[]  # name => prior
+    for d in arr
+       paramname, prior = first(d)
+       priorname, args  = lowercase(prior[1]), prior[2:end]
+       if haskey(registered_priors, priorname)
+           priordist = registered_priors[priorname]
+           push!(result, paramname => priordist(args...))
+       else
+           error("Unrecognised prior: $(prior)")
+       end
+    end
+    result
 end
 
-function format_param_unknowns!(result, n_unknowns, d)
-    if haskey(d, "params")
-        result[:params] = Symbol[]
-        params = d["params"]
-        if params isa String
-            n_unknowns += 1
-            push!(result[:params], Symbol(params))
-        elseif params isa Vector{String}
-            for x in params
-                n_unknowns += 1
-                push!(result[:params], Symbol(x))
-            end
+function particle_to_params_and_policies!(particle, name2prior, cfg, modelparams)
+    j = 0
+    policies = (:distancingpolicy, :testingpolicy, :tracingpolicy, :quarantinepolicy)
+    for (name, prior) in name2prior
+        j += 1
+        nameparts  = lowercase.(split(name, '.'))  # [category, name, date (optional)]
+        category   = Symbol(nameparts[1])
+        paramname  = Symbol(nameparts[2])
+        paramvalue = particle[j]
+        if category == :params
+            modelparams[paramname] = paramvalue
+        elseif in(category, policies)
+            date      = Date(nameparts[3])
+            fld       = Symbol("t2$(category)")
+            dt2policy = getfield(cfg, fld)
+            policy    = dt2policy[date]
+            paramname = hasfield(typeof(policy), paramname) ? paramname : Symbol(uppercase(String(paramname)))
+            setfield!(policy, paramname, paramvalue)
         else
-            error("Type of unknown params must be String or Vector{String}")
+            error("Unrecognised parameter category: $(category)")
         end
     end
-    n_unknowns
-end
-
-function format_policy_unknowns!(result, n_unknowns, d, policyname_str::String)
-    !haskey(d, policyname_str) && return n_unknowns
-    d2 = d[policyname_str]
-    isempty(d2) && return n_unknowns
-    policyname = Symbol(replace(policyname_str, "_" => ""))  # Example: "distancing_policy" => :distancingpolicy
-    result[policyname] = Pair{Date, Vector{Symbol}}[]  # Using a Vector so that order is preserved
-    for (dt, flds) in d2
-        newvec = Symbol[]
-        if flds isa String
-            n_unknowns += 1
-            push!(newvec, Symbol(flds))
-        elseif flds isa Vector{String}
-            for fld in flds
-                n_unknowns += 1
-                push!(newvec, Symbol(fld))
-            end
-        else
-            error("Type of unknown policy elements must be String or Vector{String}")
-        end
-        push!(result[policyname], Date(dt) => newvec)
-    end
-    sort!(result[policyname], by = (x) -> x[1])  # Sort by date, earliest to latest. Not necessary but easier to keep track of.
-    n_unknowns
-end
-
-################################################################################
-# Converting from params and policies to theta, and back again.
-
-"Modified: theta"
-function params_and_policies_to_theta!(theta, unknowns, cfg, params)
-    i = haskey(unknowns, :params)           ? params_to_theta!(theta, 0, unknowns[:params], params) : 0
-    i = haskey(unknowns, :distancingpolicy) ? policy_to_theta!(theta, i, unknowns[:distancingpolicy], cfg.t2distancingpolicy) : i
-    i = haskey(unknowns, :testingpolicy)    ? policy_to_theta!(theta, i, unknowns[:testingpolicy],    cfg.t2testingpolicy)    : i
-    i = haskey(unknowns, :tracingpolicy)    ? policy_to_theta!(theta, i, unknowns[:tracingpolicy],    cfg.t2tracingpolicy)    : i
-    i = haskey(unknowns, :quarantinepolicy) ? policy_to_theta!(theta, i, unknowns[:quarantinepolicy], cfg.t2quarantinepolicy) : i
-end
-
-"Modified: params, cfg."
-function theta_to_params_and_policies!(theta, unknowns, cfg, params)
-    i = haskey(unknowns, :params)           ? theta_to_params!(theta, 0, unknowns[:params], params) : 0
-    i = haskey(unknowns, :distancingpolicy) ? theta_to_policy!(theta, i, unknowns[:distancingpolicy], cfg.t2distancingpolicy) : i
-    i = haskey(unknowns, :testingpolicy)    ? theta_to_policy!(theta, i, unknowns[:testingpolicy],    cfg.t2testingpolicy)    : i
-    i = haskey(unknowns, :tracingpolicy)    ? theta_to_policy!(theta, i, unknowns[:tracingpolicy],    cfg.t2tracingpolicy)    : i
-    i = haskey(unknowns, :quarantinepolicy) ? theta_to_policy!(theta, i, unknowns[:quarantinepolicy], cfg.t2quarantinepolicy) : i
-end
-
-function params_to_theta!(theta, i, unknowns::Vector{Symbol}, params)
-    for nm in unknowns
-        i += 1
-        theta[i] = params[nm]
-    end
-    i
-end
-
-function theta_to_params!(theta, i, unknowns::Vector{Symbol}, params::Dict{Symbol, Float64}, )
-    for nm in unknowns
-        i += 1
-        params[nm] = theta[i]
-    end
-    i
-end
-
-function policy_to_theta!(theta, i, unknowns::Vector{Pair{Date, Vector{Symbol}}}, dt2policy)
-    for (dt, flds) in unknowns
-        policy = dt2policy[dt]
-        for fld in flds
-            i += 1
-            theta[i] = getfield(policy, fld)
-        end
-    end
-    i
-end
-
-function theta_to_policy!(theta, i, unknowns::Vector{Pair{Date, Vector{Symbol}}}, dt2policy)
-    for (dt, flds) in unknowns
-        policy = dt2policy[dt]
-        for fld in flds
-            i += 1
-            setfield!(policy, fld, theta[i])
-        end
-    end
-    i
 end
 
 ################################################################################
@@ -206,9 +134,9 @@ end
 # Loss function
 
 function loss(particle)
-    theta_to_params_and_policies!(particle, store[:unknowns], store[:config], store[:model].params)
+    particle_to_params_and_policies!(particle, store[:name2prior], store[:config], store[:model].params)
     yhat = manyruns(store[:model], store[:config], store[:metrics], store[:ymax])
-    !isfinite(yhat[1, 1]) && return Inf  # Randomly large loss
+    !isfinite(yhat[1, 1]) && return Inf
     agg = [quantile(view(yhat, t, :), 0.5) for t = 1:size(yhat, 1)]  # Median of simulated values at each time step
     rmse(store[:y], agg)
 end
@@ -255,10 +183,9 @@ function prepare_training_data(d)
     result
 end
 
-function construct_result(particles, loglik, sd)
+function construct_result(particles, loglik, sd, colnames)
     nparticles = size(particles, 1)
     nparams    = length(particles[1])
-    colnames   = [Symbol("x$(j)") for j = 1:nparams]  # FIX
     result     = DataFrame(logprior=fill(0.0, nparticles), logposterior=fill(0.0, nparticles), loss=fill(0.0, nparticles))
     for colname in colnames
         result[!, colname] = fill(0.0, nparticles)
