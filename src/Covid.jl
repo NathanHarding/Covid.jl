8dc5c0f8e768331af240d378f60d15f7c5c4473d
jock.lawrie <jock.lawrie@gmail.com>
2020-07-06 12:09:47 +1000

    Drafted posterior estimation using SparseGrids.

--- src/Covid.jl
+++ src/Covid.jl
@@ -5,8 +5,9 @@ using .run_model
 
 #include("train_model.jl")  # Exports: trainmodel
 #include("train_model_abc.jl")  # Exports: trainmodel
-include("train_model_abc1p4.jl")  # Exports: trainmodel. Uses KissABC.jl version 1.4.
+#include("train_model_abc1p4.jl")  # Exports: trainmodel. Uses KissABC.jl version 1.4.
 #include("train_model_abc_beaumont.jl")  # Exports: trainmodel
+include("train_model_sparsegrids.jl")  # Exports trainmodel
 using .train_model
 
 end
