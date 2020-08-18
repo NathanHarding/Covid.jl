4f0cee2531f0ed5fd033fab03d647f1d1a47ea5d
NathanHarding <nhar5132@uni.sydney.edu.au>
2020-08-18 10:17:50 +1000

    updated project.toml for dependencies

--- scripts/runmodel.jl
+++ scripts/runmodel.jl
@@ -1,5 +1,9 @@
 using Pkg
 Pkg.activate(pwd())
 Pkg.instantiate()
+
+import Pkg
+Pkg.activate(normpath(joinpath(@__DIR__, "..")))
+Pkg.instantiate()
 using Covid
 Covid.runmodel(joinpath(pwd(), "config", "config.yml"))
